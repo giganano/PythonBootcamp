7f6d845b3a13eb8b6d12602ab58d43e6e86eaebe
astrobeard <giganano9@gmail.com>
2021-05-18 17:00:55 -0400

    slides: added example on sub-classing int

---
+++ examples/classes/positive.py
@@ -0,0 +1,30 @@
+
+class positive(int): 
+
+	""" 
+	A positive definite integer. 
+	""" 
+
+	def __new__(cls, value, *args, **kwargs): 
+		if value < 0: raise ValueError("Must be non-negative.") 
+		return super().__new__(cls, value, *args, **kwargs) 
+
+	def __add__(self, other): 
+		return self.__class__(super().__add__(other)) 
+
+	def __sub__(self, other): 
+		return self.__class__(super().__sub__(other)) 
+
+	def __mul__(self, other): 
+		return self.__class__(super().__mul__(other)) 
+
+	def __div__(self, other): 
+		return self.__class__(super().__div__(other)) 
+
+	def __str__(self): 
+		return "%d" % (int(self)) 
+
+	def __repr__(self): 
+		return "positive(%d)" % (int(self)) 
+
+
